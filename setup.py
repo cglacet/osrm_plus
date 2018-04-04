5c790e262c373aa561f40d8ef4c2e5774b7504a0
Christian Glacet <christian.glacet@gmail.com>
2018-04-04 18:00:34 +0200

    woops bug

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='osrm_plus',
-      version='0.21',
+      version='0.22',
       description='Allow to retrieve both distance and duration matrix for an input list of OSRM coordinates.',
       url='https://github.com/cglacet/osrm_plus',
       author='Christian Glacet',
