1395d8ddfbe89c6e360d5c89363c49c6c14fd641
Christian Glacet <christian.glacet@gmail.com>
2018-04-04 18:08:00 +0200

    changing version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='osrm_plus',
-      version='0.22',
+      version='0.23',
       description='Allow to retrieve both distance and duration matrix for an input list of OSRM coordinates.',
       url='https://github.com/cglacet/osrm_plus',
       author='Christian Glacet',
